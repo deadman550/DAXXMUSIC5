1a3dca2021e404081e04e41243bd92c871c649c7
deadman550 <150525864+deadman550@users.noreply.github.com>
2023-12-26 14:31:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/deadman550/DAXXMUSIC",
+    "https://github.com/deadman550/DAXXMUSIC5",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
